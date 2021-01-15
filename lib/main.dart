140b2af8ab0c5ce5291aaaffa0a0cd89f2793f47
idoalit <ido.alit@gmail.com>
2021-01-15 20:38:56 +0700

    Re-design

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,132 @@
-import 'package:bibliography/ui/home.dart';
-import 'package:flutter/material.dart';
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'view_model/counter.dart';
+
+void main() {
+  runApp(
+      // Provide the model to all widgets within the app. We're using
+      // ChangeNotifierProvider because that's a simple way to rebuild
+      // widgets when a model changes. We could also just use
+      // Provider, but then we would have to listen to Counter ourselves.
+      //
+      // Read Provider's docs to learn about all the available providers.
+      MultiProvider(
+    providers: [ChangeNotifierProvider(create: (context) => Counter())],
+    child: MyApp(),
+  ));
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Library',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'My Library',
+          style: TextStyle(color: Colors.black54),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            // Consumer looks for an ancestor Provider widget
+            // and retrieves its model (Counter, in this case).
+            // Then it uses that model to build widgets, and will trigger
+            // rebuilds if the model is updated.
+            Consumer<Counter>(
+              builder: (context, counter, child) => Text(
+                '${counter.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 5.0,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              color: Colors.redAccent,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.favorite),
+              color: Colors.lightGreen,
+              onPressed: () {},
+            ),
+            Text(''),
+            IconButton(
+              icon: Icon(Icons.settings),
+              color: Colors.deepOrangeAccent,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              color: Colors.indigo,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
     );
   }
 }
